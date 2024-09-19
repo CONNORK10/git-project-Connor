b9c3792e8705a25f0a85b12abc2b1bd6fe905fc9
Connor <CKIM7@HWEMAIL.COM>
2024-09-19 12:56:59 -0700

    Started the creation of makeGitDirectory
    wrote instructions and the skeleton of makeGitDirectory

--- Git.java
+++ Git.java
@@ -3,6 +3,14 @@ public class Git {
 }
 
 public static void main(String[] args){
+
+}
+// Make a method which **initializes a Git repo.**  It should be able to:
+// **Create** a `git` directory if it doesn't exist
+// 1. **Create** an `objects` directory ***inside*** the `git` directory if it doesn't exist
+// 2. **Create** a file named `index` ***inside*** the `git` directory if it doesn't exist 
+//      (Note: it is NOT .txt it is simply called ‘index’)
+
+public void makeGitDirectory(){
     
 }
-
